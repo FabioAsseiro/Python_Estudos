69206879c0d45558a8dc00c07486c04cd7673828
Fábio Nogueira Asseiro <121131847+FabioAsseiro@users.noreply.github.com>
2023-06-28 17:36:27 -0300

    ExAlura.py

--- ExAlura.py
+++ ExAlura.py
@@ -2,12 +2,13 @@ print("*****************")
 print("Adivinhe o numero")
 print("*****************")
 
-numero_secreto = 25
+import random
+numero_secreto = random.randint(0, 100)
 tentativas = 5
 rodada = 1
 
 while(rodada <= tentativas):
-    print("Voce esta na rodada",rodada ,"de", tentativas )
+    print("Voce esta na rodada {} de {}".format(rodada , tentativas ))
     chute_str = input("Digite um numero: ")
     chute = int(chute_str)
 
@@ -17,15 +18,18 @@ while(rodada <= tentativas):
 
     if (correto):
         print("Acertou!")
+        tentativas = 0
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
 
-    rodada = rodada + 1
+        rodada = rodada + 1
 
 
+
+print("O numero era: ", numero_secreto)
 
 print("*****************")
 print("*  FIM DO JOGO  *")
